fade5e7106a622062e0fa5dcefcbf64e4d023262
Victor An <2378740+duoan@users.noreply.github.com>
2025-02-11 13:06:24 -0800

    spit the sample rate to train and valid. change default norm to rms

--- src/tiny_gpt/model/config.py
+++ src/tiny_gpt/model/config.py
@@ -10,7 +10,8 @@ class TinyGPTConfig(PretrainedConfig):
     model_type: str = "tiny_gpt"
     num_workers: int = 1
     epochs: int = 1
-    sample_rate: float = 0.001
+    train_sample_rate: float = 0.001
+    val_sample_rate: float = 0.001
     # adamw optimizer
     # max learning rate
     learning_rate: float = 6e-4
@@ -33,13 +34,23 @@ class TinyGPTConfig(PretrainedConfig):
     n_layers: int = 2
     n_heads: int = 2
 
+    # position encoding approach
+    # support
+    # Absolute positional encoding
+    # 1. [sinusoidal] Sinusoidal positional encoding   [TODO]
+    # 2. [learnable] Learnable positional embedding    [DONE]
+    # Relative Positional Encoding
+    # 3. [rope] Rotary position embedding (RoPE)       [TODO]
+    # 4. [alibi] ALiBi (Attention with Linear Biases)  [TODO]
+    position_enc: str = "learnable"
+
     attention_is_fast: bool = True
     attention_dropout_p: float = 0.1
 
     ffn_dropout_p: float = 0.1
     ffn_activation_fn: str = "gelu"
-
-    normalization: str = "layer"
+    # support layer norm and RMS norm
+    normalization: str = "rms"
     normalization_bias: bool = True
 
     embd_dropout_p: float = 0.1
