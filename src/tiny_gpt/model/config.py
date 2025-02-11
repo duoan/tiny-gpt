0339737d3b297b6c82057ae7dfa3c00547cc5d66
Victor An <2378740+duoan@users.noreply.github.com>
2025-02-11 14:19:34 -0800

    switch to float32 and layer norm for better performance

--- src/tiny_gpt/model/config.py
+++ src/tiny_gpt/model/config.py
@@ -9,7 +9,7 @@ n_vocab = tiktoken.get_encoding("gpt2").n_vocab
 class TinyGPTConfig(PretrainedConfig):
     model_type: str = "tiny_gpt"
     num_workers: int = 1
-    epochs: int = 1
+    epochs: int = 2
     train_sample_rate: float = 0.001
     val_sample_rate: float = 0.001
     # adamw optimizer
@@ -26,7 +26,7 @@ class TinyGPTConfig(PretrainedConfig):
     log_interval: int = 100
     eval_interval: int = 2000
     checkpoint_interval: int = 100
-    dtype: str = "float16"
+    dtype: str = "float32"
     vocab_size: int = n_vocab
     batch_size: int = 16
     seq_len: int = 128
@@ -50,7 +50,7 @@ class TinyGPTConfig(PretrainedConfig):
     ffn_dropout_p: float = 0.1
     ffn_activation_fn: str = "gelu"
     # support layer norm and RMS norm
-    normalization: str = "rms"
+    normalization: str = "layer"
     normalization_bias: bool = True
 
     embd_dropout_p: float = 0.1
